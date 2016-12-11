ac436285462f4002df558f47de689b8eb66c6d0c
Mike Shultz <mike@votesmart.org>
2016-12-10 19:35:13 -0700

    Added header comments to python files.

--- pygsm.py
+++ pygsm.py
@@ -1,3 +1,23 @@
+"""
+pygsm - A RESTful API that acts as a game server tracker
+
+Copyright (C) 2016 GoInto, LLC
+
+This file is part of pygsm.
+
+pygsm is free software: you can redistribute it and/or modify it under 
+the terms of the GNU General Public License as published by the Free 
+Software Foundation, either version 2 of the License, or (at your option)
+any later version.
+
+pygsm is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU General Public License for more details. You should have 
+received a copy of the GNU General Public License along with pygsm. If 
+not, see <http://www.gnu.org/licenses/>.
+"""
 import hug
 from psycopg2.extras import Json
 from marshmallow import fields
